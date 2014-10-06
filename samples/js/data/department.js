0e0476d726fda05e6480c314b31425c3350095d7
Marina-L-Stoyanova <m_stoianova_9@abv.bg>
2014-10-06 17:13:06 +0300

    Added new samples

---
+++ samples/js/data/department.js
@@ -0,0 +1,13 @@
+﻿app.factory('department', [function () {
+    var src = {
+        data:[
+            { Budget: 30, Department: "Administration" },
+            { Budget: 50, Department: "Sales" },
+            { Budget: 60, Department: "IT" },
+            { Budget: 50, Department: "Marketing" },
+            { Budget: 100, Department: "Development" },
+            { Budget: 20, Department: "Support" }
+        ]
+    };
+    return src;
+}]);
